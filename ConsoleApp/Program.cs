3ea44d5fde2e1e1e1b83c3dba5db0eadc0d6e22a
Hiromi Acosta Quevedo <mafequevedo1@gmail.com>
2022-10-30 19:17:20 -0500

    Git y gitignore
    Explicación git y gitignore

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+/* ¿Qué es Git?
+ * Git es una herramienta para realizar funciones de control de versiones de código de forma distribuida.
+ * El autor del diseño es Linus Torvalds. No depende de un repositorio central. Es software libre. Mantiene un historial de versiones completo, lo que facilita la revisión del código de una manera muy simplificada. Es rápido. Funciona con ramas, lo que hace que la distribución y la mezcla sean muy fáciles. Las ramas son para mantener múltiples proyectos dentro del proyecto maestro, permite probar sistemas o nuevas funciones. También pueden tener diferentes líneas de progresión desde la rama principal y esto se puede pasar a la rama principal de desarrollo. Se pueden probar algunos de estos ajustes o cambios de código y fusionarlos en el proyecto principal ya que Git maneja todo de manera eficiente.
+ * ¿Para qué sirve el .gitignore en un repositorio?
+ * Git está diseñado para que las ramas y las etiquetas sean el proceso principal (a diferencia de SVN), y las acciones que afectan las ramas y las etiquetas (como fusionar o revertir) también se almacenan en el historial de cambios.
+*/
